884ade6a4cebc4e6d50eb4e896d34e99027ba9e7
MustafaKaanKoc <m.kaankoc8@gmail.com>
2026-05-11 00:22:49 +0200

    Added video tracking

--- test_vide_intelligence.py
+++ test_vide_intelligence.py
@@ -8,7 +8,7 @@ os.makedirs(output_dir, exist_ok=True)
 
 model = YOLO("yolov8n.pt")
 
-video_path = "/Users/burakberkerbasergun/Desktop/objectDetection/Screen Recording 2026-05-10 at 22.08.56.mov"
+video_path = "Screen Recording 2026-05-10 at 22.08.56.mov"
 cap = cv2.VideoCapture(video_path)
 
 if not cap.isOpened():
@@ -69,9 +69,11 @@ def preprocess_pipeline(image):
 
 # Detection
 def run_detection_with_boxes(image):
+
     results = model(image)
 
     detections = []
+
     for box in results[0].boxes:
         cls = int(box.cls[0])
         conf = float(box.conf[0])
@@ -91,7 +93,10 @@ def run_detection_with_boxes(image):
 def draw_detections(image, detections):
     for det in detections:
         x1, y1, x2, y2 = map(int, det["bbox"])
-        label = f"{det['class']} {det['confidence']:.2f}"
+        if "track_id" in det:
+            label = f"ID:{det['track_id']} {det['class']} {det['confidence']:.2f}"
+        else:
+            label = f"{det['class']} {det['confidence']:.2f}"
 
         cv2.rectangle(image, (x1, y1), (x2, y2), (0,255,0), 2)
         cv2.putText(image, label, (x1, y1-10),
@@ -128,28 +133,63 @@ stats = {
 # Main Loop
 
 frame_count = 0
-
+id_map = {}
+next_display_id = 1
 while frame_count < 100:
+
     ret, frame = cap.read()
 
     if not ret or frame is None:
-        print(" Frame read failed, stopping")
+        print("Frame read failed, stopping")
         break
 
-    # ORIGINAL
-    orig_det = run_detection_with_boxes(frame)
+    # TRACKING
+    track_results = model.track(
+        frame,
+        persist=True,
+        tracker="bytetrack.yaml"
+    )
+
+    orig_det = []
+
+    for box in track_results[0].boxes:
+
+        cls = int(box.cls[0])
+        conf = float(box.conf[0])
+        xyxy = box.xyxy[0].tolist()
+
+        display_id = -1
+
+        if box.id is not None:
+
+            real_id = int(box.id[0])
+
+            # Assign ONLY ONCE
+            if real_id not in id_map:
+                id_map[real_id] = next_display_id
+                next_display_id += 1
+
+            display_id = id_map[real_id]
+
+        orig_det.append({
+            "class": model.names[cls],
+            "confidence": conf,
+            "bbox": xyxy,
+            "track_id": display_id
+        })
+
     stats["original"].append(summarize(orig_det))
 
     frame_with_boxes = draw_detections(frame.copy(), orig_det)
 
-    # PROCESSED
+    # PREPROCESSING
     processed = preprocess_pipeline(frame)
 
     for name, img in processed.items():
         det = run_detection_with_boxes(img)
         stats[name].append(summarize(det))
 
-    # Visualization (equalized)
+    # Visualization
     eq_det = run_detection_with_boxes(processed["equalized"])
     eq_frame = draw_detections(processed["equalized"].copy(), eq_det)
 
